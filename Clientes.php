8d82870237646d70ce692d50981691d1a45abbfc
Pablo George <pablogeokar@hotmail.com>
2016-04-02 20:00:11 -0300

    Integração com Bootstrap

--- Clientes.php
+++ Clientes.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 class Clientes
 {
     private $codigo;
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+    require_once "Clientes.php";
+
+
+//Declaração do Array proposto com 10 clientes
 $relacaoClientes = array(
     ['nome'=> "Pablo George", 'cpf'=>"123456789101", 'cidade'=>"Feira de Santana/BA", 'endereco'=>"Endereco A"],
     ['nome'=> "Maria da Silva", 'cpf'=>"123456789101", 'cidade'=>"Feira de Santana/BA", 'endereco'=>"Endereco A"],
@@ -12,13 +16,69 @@ $relacaoClientes = array(
     ['nome'=> "jesus", 'cpf'=>"123456789101", 'cidade'=>"Feira de Santana/BA", 'endereco'=>"Endereco A"],
     ['nome'=> "Tonhao", 'cpf'=>"123456789101", 'cidade'=>"Feira de Santana/BA", 'endereco'=>"Endereco A"]
 );
-$cod = 1;
 
-foreach ($relacaoClientes as $cliente){
-    $cod = $cod++;
-    $nome = $cliente['nome'];
-    $cpf = $cliente['cpf'];
-    $cidade = $cliente['cidade'];
-    $endereco = $cliente['endereco'];
-    echo "<a href='cliente-info.php?cod=$cod&nome=$nome&cpf=$cpf&cidade=$cidade&endereco=$endereco'>$nome</a></br>";
-}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHPoo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Relação de Clientes</h2>
+        <?php            
+            $cod = 0;
+
+
+            foreach ($relacaoClientes as $cliente){
+
+
+                $objClientes[$cod] = new Clientes($cod, $cliente['nome'],$cliente['cpf'],$cliente['cidade'], $cliente['endereco'] );
+
+
+                $nome = $cliente['nome'];
+
+                //echo "<a href='cliente-info.php?cod=$cod' data-toggle=\"modal\" data-target=\"#myModal\"/>$nome</a></br>";
+                //echo "<a href='#' data-toggle=\"modal\" data-target=\"#myModal\"/>$nome</a></br>";
+                echo "<a href='#' data-toggle=\"modal\" data-target=\"#myModal$cod\">$nome</a></br>";
+
+
+         ?>
+                <!-- Modal -->
+                <div class="modal fade" id="<?php echo 'myModal'.$cod ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title" id="myModalLabel">Detalhes do Cliente</h4>
+                            </div>
+                            <div class="modal-body">
+                                <?php $objClientes[$cod]->exibeCliente(); ?>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+           <?php $cod++; }; ?>
+
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
+
